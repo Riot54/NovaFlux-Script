a7437a7f3c44d738b82d83d368493f84d8066615
Harrison Porteous <harrisonporteous@gmail.com>
2015-02-05 19:47:08 -0500

    Update cameraWaypoints.js

--- Camera/cameraWaypoints.js
+++ Camera/cameraWaypoints.js
@@ -1,12 +1,12 @@
-﻿#pragma strict
+#pragma strict
 private var player:GameObject;
 
 public var waypoint:Transform[];
 
 private var currentWaypoint:float = 0;
 
-private var moveSpeed:float = 18;
-private var moveSmooth:float = 10;
+public var moveSpeed:float = 18;
+public var moveSmooth:float = 10;
 private var staticCam:boolean = false;
 
 public var startMove:boolean = false;
@@ -15,8 +15,30 @@ private var offsetY:float = 3;
 
 private var theEnd:boolean = false;
 
+private var cameraPosition:int = 1;
+
+private var pPos1:GameObject;
+private var pPos2:GameObject;
+private var pPos3:GameObject;
+
+private var pPosHolder:GameObject;
+
+private var zoomSpeed:float = 1;
+private var zoomSmooth:float = 10;
+
+private var modZoom:float = 60;
+private var currentZoom:float = 60;
+
+
+private var cameraMain:Camera;	
+
 function Start () {
 	player = GameObject.FindWithTag("Player");
+	pPosHolder = GameObject.Find("pPosHolder");
+	pPos1 = GameObject.Find("pPos1");
+	pPos2 = GameObject.Find("pPos2");
+	pPos3 = GameObject.Find("pPos3");
+	cameraMain = GameObject.FindWithTag("MainCamera").camera;
 }
 
 function Update() {
@@ -24,9 +46,29 @@ function Update() {
 		Debug.DrawLine(waypoint[i-1].transform.position, waypoint[i].transform.position);
 	}
 	var camModScript: cameraMods = waypoint[currentWaypoint].GetComponent(cameraMods); 
+	cameraPosition = camModScript.camPosition;
 	moveSpeed = camModScript.moveSpeed;
 	moveSmooth = camModScript.smooth;
 	staticCam = camModScript.staticCam;
+	modZoom = camModScript.zoom;
+
+	if(cameraPosition == 1){
+		pPosHolder.transform.parent = pPos1.transform;
+		pPosHolder.transform.localPosition = Vector3(0,0,0);
+	}
+
+	if(cameraPosition == 2){
+		pPosHolder.transform.parent = pPos2.transform;
+		pPosHolder.transform.localPosition = Vector3(0,0,0);
+	}
+
+	if(cameraPosition == 3){
+		pPosHolder.transform.parent = pPos3.transform;
+		pPosHolder.transform.localPosition = Vector3(0,0,0);
+	}
+
+	//cameraMain.fieldOfView = Mathf.Lerp(currentZoom, modZoom, Time.deltaTime * zoomSmooth);
+	Debug.Log(currentZoom);
 }
 
 function FixedUpdate(){
@@ -43,10 +85,11 @@ function FixedUpdate(){
 		var wpDistance = Vector3.Distance(target, transform.position);
 
 		var dir : Vector3 = (target - transform.position);
-		var dirNorm : Vector3 = (target - transform.position).normalized * 20;
+		var dirNorm : Vector3 = (target - transform.position).normalized * moveSpeed;
 		if(!staticCam){
 			if (wpDistance <= 1){
 				if(!theEnd){
+					currentZoom = modZoom;
 					currentWaypoint += 1;
 				}
 			}else{
